7a75cf318f84df15f4da7165f964149496371212
yin.zhu <yin.zhu@qunar.com>
2019-10-12 09:38:37 +0800

    add

---
+++ app/src/main/cpp/jni/common.h
@@ -0,0 +1,9 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+#include <android/log.h>
+
+#define LOG_TAG "snow_soHook"
+#define LOGE(...)    __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+#endif
